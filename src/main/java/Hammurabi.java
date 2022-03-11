1174aaf3b4f5efe49364bf4c3dac71ce69c2fbca
Clyde Broderick <clyderick3@gmail.com>
2022-03-11 17:50:56 -0500

    Added pom.xml file.

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -1,2 +1,9 @@
-package hammurabi.src.main.java;public class Hammurabi {
+package hammurabi.src.main.java;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hammurabi {
+    Random rand = new Random();
+    Scanner scanner = new Scanner(System.in);
 }
