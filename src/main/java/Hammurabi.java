f53e2bb835b11890921e3c891976835d61223b7e
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-12 15:26:38 -0500

    plaguedeath and rategrains

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -6,4 +6,19 @@ import java.util.Scanner;
 public class Hammurabi {
     Random rand = new Random();
     Scanner scanner = new Scanner(System.in);
+
+    public int plagueDeath (int population) {
+        return randomChance(15) ? population/2 : population;
+    }
+
+    public int grainEatenByRats(int bushels){
+        return randomChance(40)
+                ? (100-(rand.nextInt(20)+10))*bushels / 100
+                :bushels;
+    }
+
+    public boolean randomChance(double chanceOfOccurrence){
+        Random rand = new Random();
+        return rand.nextDouble()>(chanceOfOccurrence/100);
+    }
 }
