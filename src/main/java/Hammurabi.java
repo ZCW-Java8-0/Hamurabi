e768e291d69eed1a5d72fab3cc98e90ebb6618a8
Clyde Broderick <clyderick3@gmail.com>
2022-03-12 15:39:41 -0500

    Completed our scan1.

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -18,20 +18,30 @@ public class Hammurabi {
         int land = 1000;
         int valueBushelsPerAcre = 19; // land value is 19 bushels/acre
         int years = 10;
+    }
 
-        for (int i = 0; i < years ; i ++) {
+        public static int askHowManyAcresToBuy (int price, int bushels) {
+            Scanner scan1 = new Scanner (System.in);
+            System.out.println("How many acres would you like to buy?");
+            //Buying =
+            int numberOfAcresToBuy = scan1.nextInt();
 
-
-            System.out.println("O great Hammurabi!\n" +
-                    "You are in year 1 of your ten year rule.\n" +
-                    "In the previous year 0 people starved to death.\n" +
-                    "In the previous year 5 people entered the kingdom.\n" +
-                    "The population is now 100.\n" +
-                    "We harvested 3000 bushels at 3 bushels per acre.\n" +
-                    "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
-                    "The city owns 1000 acres of land.\n" +
-                    "Land is currently worth 19 bushels per acre.");
         }
+
+
+        //for (int i = 0; i < years ; i ++) {
+
+
+//            System.out.println("O great Hammurabi!\n" +
+//                    "You are in year 1 of your ten year rule.\n" +
+//                    "In the previous year 0 people starved to death.\n" +
+//                    "In the previous year 5 people entered the kingdom.\n" +
+//                    "The population is now 100.\n" +
+//                    "We harvested 3000 bushels at 3 bushels per acre.\n" +
+//                    "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
+//                    "The city owns 1000 acres of land.\n" +
+//                    "Land is currently worth 19 bushels per acre.");
+
 
 
         //Ask the questions in this order. No backing up. No changing answers.
