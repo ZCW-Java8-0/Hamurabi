b36ca52a6f458b6d61a1c783b8901e6ea2728474
MilesConey <99622565+MilesConey@users.noreply.github.com>
2022-03-13 16:30:32 -0400

    Revert "Dev"

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Hammurabi {
     Random rand = new Random();
     Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
         new Hammurabi().playGame();
     }
 
@@ -13,27 +15,7 @@ public class Hammurabi {
         int grain = 2800; //bushels of grain in storage
         int land = 1000;
         int valueBushelsPerAcre = 19; // land value is 19 bushels/acre
-        int years = 1;
-        int bought, acresPlanted,feed,plague,starved,immig,harvest,rats;
-        while (years<=10) {
-            printSummary();
-            bought=askHowManyAcresToBuy(valueBushelsPerAcre, grain);
-            if (bought>0)
-                land+=bought;
-            else
-                land-=askHowManyAcresToSell(valueBushelsPerAcre,grain);
-            feed=askHowMuchGrainToFeedPeople(grain);
-            acresPlanted=askHowManyAcresToPlant(land, population, grain);
-            plague=plagueDeaths(population);
-            starved=starvationDeaths(population,feed);
-            immig=immigrants(population,land,grain);
-            harvest=harvest(acresPlanted);
-            rats=grainEatenByRats(grain);
-            valueBushelsPerAcre=newCostOfLand();
-            population=population-plague-starved+immig;
-            grain=grain-rats-feed+harvest;
-            years++;
-        }
+        int years = 10;
     }
 
         public static int askHowManyAcresToBuy (int price, int bushels) {
@@ -74,25 +56,4 @@ public class Hammurabi {
         } else if (acresToPlant < 2 * bushels)
     }
 
-    public int plagueDeaths (int population) {
-        return randomChance(15) ? population/2 : 0;
-    }
-
-    public int newCostOfLand(){
-        return rand.nextInt(7)+17; //bound is exclusive
-    }
-
-    public int harvest(int acresPlantedWithSeeds){
-        return (rand.nextInt(6)+1)*acresPlantedWithSeeds;
-    }
-
-    public int grainEatenByRats(int bushels){
-        return randomChance(40)
-                ? (rand.nextInt(21)+10)*bushels / 100 //bound is exclusive
-                :0;
-    }
-
-    public boolean randomChance(double chanceOfOccurrence){
-        return rand.nextDouble()<(chanceOfOccurrence/100);
-    }
 }
